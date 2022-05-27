7c4aaececc861ccde815e79b9d2df80bf9c8ba88
Olle <hugo.berggren@gmail.com>
2022-05-27 02:33:21 +0200

    Update ob-dice.py
    comments and descriptions

--- ob-dice.py
+++ ob-dice.py
@@ -1,10 +1,13 @@
 #!/bin/python3
+# ob-dice.py
+# Roll T6 dice, reroll two dice when rolling a 6.
+# Used in the Swedish tabletop/pen & paper RPG Eon
 
 import random
 import argparse
 
 # Argparse
-parser = argparse.ArgumentParser(description='EON OB dice roller.')
+parser = argparse.ArgumentParser(description='EON ob dice roller.')
 parser.add_argument('--t6', type=int, required=False, default=1)
 parser.add_argument('--bonus', type=int, required=False, default=0)
 args = parser.parse_args()
